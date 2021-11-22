8bd56ad26272a946a508650a1dafc70de32c6cb6
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2021-11-22 21:46:09 +0100

    add PrintService ViewModel

---
+++ Logic.Ui/ViewModels/PrintServiceWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using De.HsFlensburg.ClientApp011.Logic.Ui.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp011.Logic.Ui.ViewModels
+{
+    public class PrintServiceWindowViewModel
+    {
+        public BookCollectionViewModel BookList { get; set; }
+    }
+}
