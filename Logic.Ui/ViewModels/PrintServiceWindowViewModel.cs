89e0def9d1e62417a5fa98cad3c9805816e749bd
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2021-11-29 12:12:16 +0100

    add printing functions

--- Logic.Ui/ViewModels/PrintServiceWindowViewModel.cs
+++ Logic.Ui/ViewModels/PrintServiceWindowViewModel.cs
@@ -1,14 +1,58 @@
 ﻿using De.HsFlensburg.ClientApp011.Logic.Ui.Wrapper;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace De.HsFlensburg.ClientApp011.Logic.Ui.ViewModels
 {
     public class PrintServiceWindowViewModel
     {
+        private int selectedBookIndex = 0;
+        public int SelectedBookIndex 
+        { 
+            get
+            {
+                return selectedBookIndex;
+            }
+            set
+            {
+                selectedBookIndex = value;
+                Console.WriteLine(BookList[selectedBookIndex].Title);
+                CurrentBook = BookList[selectedBookIndex];
+            }
+        }
+
         public BookCollectionViewModel BookList { get; set; }
+        public BookViewModel CurrentBook { get; set; }
+        public ICommand PrintBooks { get; }
+
+        public PrintServiceWindowViewModel(BookCollectionViewModel bookCollectionViewModel)
+        {
+            PrintBooks = new RelayCommand(PrintBooksCommand);
+            BookList = bookCollectionViewModel;
+            CreateDemoBooks();
+        }
+
+        private void CreateDemoBooks()
+        {
+            Console.WriteLine("Create Demo Books...");
+
+            // DEMO BOOKS
+            for (int i = 1; i <= 10; i++)
+            {
+                BookViewModel book = new BookViewModel();
+                book.Title = "Book #" + i;
+                BookList.Add(book);
+            }
+        }
+
+        private void PrintBooksCommand()
+        {
+            Console.WriteLine("print...");
+        }
     }
 }
--- Services.PrintService/PrintBookService.cs
+++ Services.PrintService/PrintBookService.cs
@@ -3,46 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 
 namespace De.HsFlensburg.ClientApp011.Services.PrintService
 {
-    public class PrintBookService
+    class PrintBookService
     {
-        public FlowDocument flowDocument;
-
-        public PrintBookService()
-        {
-            flowDocument = new FlowDocument();
-            flowDocument.MinPageHeight = 680.0;
-            flowDocument.MaxPageHeight = 480.0;
-        }
-
-        public FlowDocument BookDocument()
-        {
-            Paragraph paragraph = new Paragraph();
-
-            Run header = new Run("Übersicht");
-            header.FontFamily = new System.Windows.Media.FontFamily("Arial");
-            header.FontSize = 24;
-            paragraph.Inlines.Add(header);
-            flowDocument.Blocks.Add(paragraph);
-
-            return flowDocument;
-        }
-
-        public void Printing()
-        {
-            PrintDialog printDialog = new PrintDialog();
-            IDocumentPaginatorSource idpSource = flowDocument;
-
-            Nullable<Boolean> print = printDialog.ShowDialog();
-            if(print == true)
-            {
-                printDialog.PrintDocument(idpSource.DocumentPaginator, "BookPrint");
-            }
-        }
     }
 }
