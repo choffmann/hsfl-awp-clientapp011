2c4425d95928b48bcffcfd3a680c6b9e7ab9e320
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2021-12-13 14:15:35 +0100

    clean up code | delete clientcollection and co.

--- Logic.Ui/ViewModelLocator.cs
+++ Logic.Ui/ViewModelLocator.cs
@@ -10,19 +10,14 @@ namespace De.HsFlensburg.ClientApp011.Logic.Ui
 {
     public class ViewModelLocator
     {
-        public ClientCollectionViewModel TheClientCollectionViewModel { get; set; }
         public MainWindowViewModel TheMainWindowViewModel { get; set; }
-        public NewClientWindowViewModel TheNewClientWindowViewModel { get; set; }
         public BookCollectionViewModel TheBookCollectionViewModel { get; set; }
         public NewBookViewModel TheNewBookViewModel { get; set; }
 
         public ViewModelLocator()
         {
-            TheClientCollectionViewModel = new ClientCollectionViewModel();
-            TheMainWindowViewModel = new MainWindowViewModel(TheClientCollectionViewModel);
-            TheNewClientWindowViewModel = new NewClientWindowViewModel(TheClientCollectionViewModel);
-            
             TheBookCollectionViewModel = new BookCollectionViewModel();
+            TheMainWindowViewModel = new MainWindowViewModel(TheBookCollectionViewModel);
             TheNewBookViewModel = new NewBookViewModel(TheBookCollectionViewModel);
         }
     }
