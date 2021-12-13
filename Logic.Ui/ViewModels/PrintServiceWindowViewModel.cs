b15b83614f1e8ba2a6efce576901389f05045ebc
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2021-12-13 13:19:00 +0100

    implement preview of books

--- Logic.Ui/ViewModels/PrintServiceWindowViewModel.cs
+++ Logic.Ui/ViewModels/PrintServiceWindowViewModel.cs
@@ -1,58 +1,99 @@
 ﻿using De.HsFlensburg.ClientApp011.Logic.Ui.Wrapper;
+using De.HsFlensburg.ClientApp011.Services.PrintService;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace De.HsFlensburg.ClientApp011.Logic.Ui.ViewModels
 {
-    public class PrintServiceWindowViewModel
+    public class PrintServiceWindowViewModel: INotifyPropertyChanged
     {
-        private int selectedBookIndex = 0;
-        public int SelectedBookIndex 
-        { 
+        public BookCollectionViewModel BookList { get; set; }
+        public BookCollectionViewModel CheckedBooks { get; set; }
+        public ICommand PrintBooks { get; }
+
+        private bool isSelected;
+        public bool IsSelected
+        {
             get
             {
-                return selectedBookIndex;
+                return isSelected;
             }
             set
             {
-                selectedBookIndex = value;
-                Console.WriteLine(BookList[selectedBookIndex].Title);
-                CurrentBook = BookList[selectedBookIndex];
+                if(isSelected != value)
+                {
+                    isSelected = value;
+                    OnPropertyChanged("IsSelected");
+                }
             }
         }
-
-        public BookCollectionViewModel BookList { get; set; }
-        public BookViewModel CurrentBook { get; set; }
-        public ICommand PrintBooks { get; }
 
         public PrintServiceWindowViewModel(BookCollectionViewModel bookCollectionViewModel)
         {
             PrintBooks = new RelayCommand(PrintBooksCommand);
             BookList = bookCollectionViewModel;
+            CheckedBooks = new BookCollectionViewModel();
             CreateDemoBooks();
         }
 
         private void CreateDemoBooks()
         {
-            Console.WriteLine("Create Demo Books...");
-
             // DEMO BOOKS
+            Console.WriteLine("Create Demo Books...");
             for (int i = 1; i <= 10; i++)
             {
                 BookViewModel book = new BookViewModel();
                 book.Title = "Book #" + i;
+                book.Author = "Author #" + i;
+                book.Publisher = "Publisher #" + i;
+                book.Description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut";
+                book.Extract = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                book.Genre = Business.Model.BusinessObjects.ENUM.Genre.Horror;
+                book.Language = Business.Model.BusinessObjects.ENUM.Language.Deutsch;
+                book.Format = Business.Model.BusinessObjects.ENUM.Format.Taschenbuch;
+                book.Price = 2.3M;
+                book.Pages = 120;
+                book.Weight = 10;
+                book.Isbn = "ISBN " + i + "-" + i + 7645 + "-" + i + 2641 + "-" + i + 1;
+                book.Rating = 2.4;
+                book.Edition = i;
+                book.Bestseller = i % 2 == 0;
+                book.ReleaseDate = new DateTime();
+
+                Business.Model.BusinessObjects.Dimension dimension = new Business.Model.BusinessObjects.Dimension();
+                dimension.Depth = 1 * (i + 1);
+                dimension.Height = 2 * (i + 1);
+                dimension.Width = 3 * (i + 1);
+                book.Dimension = dimension;
+
+                Image image = Image.FromFile(@"C:\Users\hoffmann\Documents\FH Flensburg\AWP\image\" + i + ".png");
+                book.Cover = image;
                 BookList.Add(book);
             }
         }
 
         private void PrintBooksCommand()
         {
-            Console.WriteLine("print...");
+            PrintBookService printer = new PrintBookService(CheckedBooks.Model);
+            printer.Printing();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
--- Services.PrintService/PrintBookService.cs
+++ Services.PrintService/PrintBookService.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using De.HsFlensburg.ClientApp011.Business.Model.BusinessObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace De.HsFlensburg.ClientApp011.Services.PrintService
 {
@@ -11,24 +15,58 @@ namespace De.HsFlensburg.ClientApp011.Services.PrintService
     {
         public FlowDocument flowDocument;
 
-        public PrintBookService()
+        public PrintBookService(BookCollection bookCollection)
         {
             flowDocument = new FlowDocument();
             flowDocument.MinPageHeight = 680.0;
             flowDocument.MaxPageHeight = 480.0;
+            BookDocument(bookCollection);
         }
 
-        public FlowDocument BookDocument()
+        public FlowDocument BookDocument(BookCollection bookCollection)
         {
             Paragraph paragraph = new Paragraph();
 
+            FontFamily arial = new FontFamily("Arial");
+            FontFamily consolas = new FontFamily("Consolas");
+
             Run header = new Run("Übersicht");
-            header.FontFamily = new System.Windows.Media.FontFamily("Arial");
+            header.FontFamily = new FontFamily("Arial");
             header.FontSize = 24;
             paragraph.Inlines.Add(header);
             flowDocument.Blocks.Add(paragraph);
 
+            foreach (var book in bookCollection)
+            {
+                paragraph = new Paragraph();
+                flowDocument.PagePadding = new System.Windows.Thickness(40);
+
+                Run bookHeader = new Run("Book:");
+                bookHeader.FontFamily = new FontFamily("Times");
+                bookHeader.FontSize = 16;
+                bookHeader.FontWeight = FontWeights.Bold;
+                paragraph.Inlines.Add(bookHeader);
+
+
+                paragraph.Inlines.Add(AddLine("Name", book.Title, consolas, 10));
+                paragraph.Inlines.Add(AddLine("Author", book.Author, consolas, 10));
+
+                paragraph.Padding = new Thickness(15);
+                paragraph.BorderBrush = Brushes.Red;
+                paragraph.BorderThickness = new Thickness(6);
+
+                flowDocument.Blocks.Add(paragraph);
+            }
+
             return flowDocument;
+        }
+
+        private Run AddLine(String title, object value, FontFamily font, int size)
+        {
+            Run line = new Run("\n" + title + ": " + value);
+            line.FontFamily = font;
+            line.FontSize = size;
+            return line;
         }
 
         public void Printing()
