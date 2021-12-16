6b1924565b7d55d09c00428bd1ca0c9beda043a2
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2021-12-16 17:02:25 +0100

    Completed Printing Function

--- Services.PrintService/PrintBookService.cs
+++ Services.PrintService/PrintBookService.cs
@@ -1,12 +1,10 @@
 ﻿using De.HsFlensburg.ClientApp011.Business.Model.BusinessObjects;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media.Imaging;
 using System.Windows.Media;
 
 namespace De.HsFlensburg.ClientApp011.Services.PrintService
@@ -18,65 +16,123 @@ namespace De.HsFlensburg.ClientApp011.Services.PrintService
         public PrintBookService(BookCollection bookCollection)
         {
             flowDocument = new FlowDocument();
-            flowDocument.MinPageHeight = 680.0;
-            flowDocument.MaxPageHeight = 480.0;
+            flowDocument.PagePadding = new Thickness(15);
+            flowDocument.ColumnGap = 0;
             BookDocument(bookCollection);
         }
 
         public FlowDocument BookDocument(BookCollection bookCollection)
         {
-            Paragraph paragraph = new Paragraph();
-
+            // Define Fonts
             FontFamily arial = new FontFamily("Arial");
             FontFamily consolas = new FontFamily("Consolas");
 
-            Run header = new Run("Übersicht");
-            header.FontFamily = new FontFamily("Arial");
+            // Create header Title of List
+            Run header = new Run("Print BookList of " + bookCollection.Count + " Books" );
+            header.FontFamily = arial;
             header.FontSize = 24;
-            paragraph.Inlines.Add(header);
-            flowDocument.Blocks.Add(paragraph);
+            flowDocument.Blocks.Add(new Paragraph(header));
 
             foreach (var book in bookCollection)
             {
-                paragraph = new Paragraph();
-                flowDocument.PagePadding = new System.Windows.Thickness(40);
+                // Create 4x4 Table to show Book content
+                Table contentTable = new Table();
+                contentTable.Columns.Add(new TableColumn());
+                contentTable.Columns.Add(new TableColumn());
+                contentTable.Columns.Add(new TableColumn());
+                contentTable.Columns.Add(new TableColumn());
+                contentTable.Columns.Add(new TableColumn());
+                contentTable.RowGroups.Add(new TableRowGroup());
+                contentTable.RowGroups[0].Rows.Add(new TableRow());
+                contentTable.RowGroups[0].Rows.Add(new TableRow());
+                contentTable.RowGroups[0].Rows.Add(new TableRow());
+                contentTable.RowGroups[0].Rows.Add(new TableRow());
 
-                Run bookHeader = new Run("Book:");
-                bookHeader.FontFamily = new FontFamily("Times");
-                bookHeader.FontSize = 16;
-                bookHeader.FontWeight = FontWeights.Bold;
-                paragraph.Inlines.Add(bookHeader);
+                // First Column fill with Headers
+                TableRow currentRow = contentTable.RowGroups[0].Rows[0];
+                currentRow.FontFamily = arial;
+                currentRow.FontWeight = FontWeights.Bold;
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Title:"))));
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Author:"))));
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Genre:"))));
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Preis:"))));
 
+                // Second Column fill with Content
+                currentRow = contentTable.RowGroups[0].Rows[1];
+                currentRow.FontFamily = consolas;
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(book.Title))));
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(book.Author))));
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(book.Genre.ToString()))));
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(book.Price.ToString()))));
 
-                paragraph.Inlines.Add(AddLine("Name", book.Title, consolas, 10));
-                paragraph.Inlines.Add(AddLine("Author", book.Author, consolas, 10));
+                // Third Column fill with new Header
+                currentRow = contentTable.RowGroups[0].Rows[2];
+                currentRow.FontFamily = arial;
+                currentRow.FontWeight = FontWeights.Bold;
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Kurzbeschreibung:"))));
+                currentRow.Cells[0].ColumnSpan = 4;
 
-                paragraph.Padding = new Thickness(15);
-                paragraph.BorderBrush = Brushes.Red;
-                paragraph.BorderThickness = new Thickness(6);
+                // Second Column fill with new Content
+                currentRow = contentTable.RowGroups[0].Rows[3];
+                currentRow.FontFamily = consolas;
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At"))));
+                currentRow.Cells[0].ColumnSpan = 4;
 
-                flowDocument.Blocks.Add(paragraph);
+                // Create new 1x2 Table to display Image and content table
+                Table mainTable = new Table();
+                TableColumn imageColumn = new TableColumn();
+                mainTable.Columns.Add(imageColumn);
+                mainTable.Columns.Add(new TableColumn());
+                mainTable.RowGroups.Add(new TableRowGroup());
+                mainTable.RowGroups[0].Rows.Add(new TableRow());
+
+                // Insert Image in main table first row, first column 
+                // Insert content table in first row, second column
+                currentRow = mainTable.RowGroups[0].Rows[0];
+                Image cover = new Image();
+                cover.Source = ConvertModelToBitMapImage(book.Cover);
+                cover.Width = 150;
+                cover.Height = 150;
+                currentRow.Cells.Add(new TableCell(new BlockUIContainer(cover)));
+                currentRow.Cells.Add(new TableCell(contentTable));
+
+                // Change Table Space / Width
+                mainTable.Columns[0].Width = new GridLength(175);
+
+                // Add border to main table
+                mainTable.Padding = new Thickness(15);
+                mainTable.BorderBrush = Brushes.Gray;
+                mainTable.BorderThickness = new Thickness(2);
+
+                flowDocument.Blocks.Add(mainTable);
             }
-
             return flowDocument;
         }
 
-        private Run AddLine(String title, object value, FontFamily font, int size)
+        // Convert Image to BitmapImage to print on FlowDocument
+        private BitmapImage ConvertModelToBitMapImage(System.Drawing.Image cover)
         {
-            Run line = new Run("\n" + title + ": " + value);
-            line.FontFamily = font;
-            line.FontSize = size;
-            return line;
+            MemoryStream localMemoryStream = new MemoryStream();
+            cover.Save(localMemoryStream, System.Drawing.Imaging.ImageFormat.Png);
+            localMemoryStream.Position = 0;
+            BitmapImage localBitmapImage = new BitmapImage();
+            localBitmapImage.BeginInit();
+            localBitmapImage.StreamSource = localMemoryStream;
+            localBitmapImage.EndInit();
+            return localBitmapImage;
         }
 
+        // Execute Printing
         public void Printing()
         {
             PrintDialog printDialog = new PrintDialog();
             IDocumentPaginatorSource idpSource = flowDocument;
-
             Nullable<Boolean> print = printDialog.ShowDialog();
             if (print == true)
             {
+                // Set FlowDocument Width to Page Settings
+                flowDocument.ColumnWidth = printDialog.PrintableAreaWidth;
+                flowDocument.PageWidth = printDialog.PrintableAreaWidth;
                 printDialog.PrintDocument(idpSource.DocumentPaginator, "BookPrint");
             }
         }
